cb6ad9efa66d1ba14f3a857d62d0c76450534179
Rousseau Félix <ferousseau@et.esiea.fr>
2021-12-01 15:06:08 +0100

    Composite Logger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -5,6 +5,6 @@ import fr.lernejo.guessgame.Simulation;
 
 public class LoggerFactory {
     public static Logger getLogger(String name){
-        return new ContextualLogger(name,  new FileLogger("./src/main/java/fr/lernejo/logger/journal.txt"));
+        return new CompositeLogger(new ContextualLogger(name,  new FileLogger("./src/main/java/fr/lernejo/logger/journal.txt")), new ContextualLogger(name, new ConsoleLogger()) );
     }
 }
