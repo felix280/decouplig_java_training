fcc5f1f1fdf6699e2403756bcc1737f7de278abe
Rousseau Félix <ferousseau@et.esiea.fr>
2021-12-01 15:38:09 +0100

    Filtered Logger

--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -3,8 +3,10 @@ package fr.lernejo.logger;
 import fr.lernejo.guessgame.HumanPlayer;
 import fr.lernejo.guessgame.Simulation;
 
+import java.util.function.Predicate;
+
 public class LoggerFactory {
     public static Logger getLogger(String name){
-        return new CompositeLogger(new ContextualLogger(name,  new FileLogger("./src/main/java/fr/lernejo/logger/journal.txt")), new ContextualLogger(name, new ConsoleLogger()) );
+        return new CompositeLogger(new ContextualLogger(name, new FilteredLogger( new FileLogger("./src/main/java/fr/lernejo/logger/journal.txt"),  condition -> !name.contains("simulation"))), new ContextualLogger(name, new ConsoleLogger()));
     }
 }
